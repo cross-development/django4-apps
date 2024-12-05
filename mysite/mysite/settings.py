77fa326f983cd46bf0126c4436d1aa79ce4c8808
Vitalii Derda <derdavitaliy@gmail.com>
2024-12-05 17:10:31 +0200

    feat: added sitemap, p.165

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,6 +14,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
+
 # Application definition
 
 INSTALLED_APPS = [
@@ -25,6 +27,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps',
 ]
 
 MIDDLEWARE = [
