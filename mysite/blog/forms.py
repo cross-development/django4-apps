a8b92c377458142579b9949f1392abc0e8568d65
Vitalii Derda <derdavitaliy@gmail.com>
2024-12-10 16:07:46 +0200

    feat: added search for posts

--- mysite/blog/forms.py
+++ mysite/blog/forms.py
@@ -17,3 +17,7 @@ class CommentForm(forms.ModelForm):
         widgets = {
             'body': forms.Textarea(attrs={'style': 'resize: none;'}),
         }
+
+
+class SearchForm(forms.Form):
+    query = forms.CharField()
