409bcaa1fd4c236ba1ac3b80329b3e39aeff5e48
Vitalii Derda <derdavitaliy@gmail.com>
2025-01-12 13:53:30 +0200

    feat: added change and reset password views and templates, p.217

--- mysite/account/urls.py
+++ mysite/account/urls.py
@@ -1,11 +1,8 @@
-from django.urls import path
-from django.contrib.auth import views as auth_views
+from django.urls import path, include
 
 from . import views
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-
+    path('', include('django.contrib.auth.urls')),
     path('', views.dashboard, name='dashboard'),
 ]
