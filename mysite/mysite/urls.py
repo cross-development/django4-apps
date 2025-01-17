e04410e1ca228757d258096e5dd87e314c1b68ed
Vitalii Derda <derdavitaliy@gmail.com>
2025-01-17 17:52:29 +0200

    feat: expanded user profile, p.232

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
+from django.conf.urls.static import static
+from django.conf import settings
 
 from blog.sitemaps import PostSitemap
 
@@ -15,3 +17,6 @@ urlpatterns = [
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
          name='django.contrib.sitemaps.views.sitemap'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
